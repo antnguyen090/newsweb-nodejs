4c282c8af27f0dc8b45b28915e8ae750ee0f3eef
antnguyen090 <107045368+antnguyen090@users.noreply.github.com>
2022-12-14 22:36:49 +0700

    update

--- app/routes/frontend/index.js
+++ app/routes/frontend/index.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-router.use('/',require('./home'));
 router.use('/category', require('./category'));
 router.use('/contact', require('./contact'));
 router.use('/single', require('./single'));
@@ -9,5 +8,6 @@ router.use('/rss', require('./rss'));
 router.use('/search', require('./search'));
 router.use('/error', require('./error'));
 router.use('/auth', require('./auth'));
+router.use('/',require('./home'));
 
 module.exports = router;
